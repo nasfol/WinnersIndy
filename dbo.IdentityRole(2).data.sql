1de9677085af5c72435da31784ef667df44ad685
Nasser <adegboyefoluso@yahoo.com>
2021-07-19 15:26:55 -0400

    all

---
+++ dbo.IdentityRole(2).data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[IdentityRole] ([Id], [Name]) VALUES (N'31cb77f0-5f99-44c5-933f-dd46c1e83427', N'Admin')
---
+++ dbo.IdentityRole.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[IdentityRole] ([Id], [Name]) VALUES (N'940ec1fc-2d1b-4427-9af5-5bd84809d589', N'Admin')
